0a7950301d3456dcb1937e3a85e389c271f8b18b
migetonni <Mixu.Koski-Homi@metropolia.fi>
2022-08-29 14:57:26 +0300

    3.1

---
+++ 3.1.py
@@ -0,0 +1,6 @@
+kuhan_mitta = int(input("Kerro kuhan mitta"))
+alin_mitta = 37
+
+if kuhan_mitta <= alin_mitta:
+    print("Kuha on alimittainen, heitä se takaisin järveen")
+    print(f"Kuha on {alin_mitta - kuhan_mitta}cm liian pieni")
